57762cf6767165f5e9e26f865ff75278bb83fe41
pikupika <peeyuvi2002@gmail.com>
2025-07-28 10:52:35 +0530

    missing token

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const token = req.headers.authorization?.split(" ")[1];
+if (!token) return res.status(401).json({ message: "Missing token" });
