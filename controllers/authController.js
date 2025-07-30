b6c172fd8bc09a0221a7986f1d2775053488daa3
pikupika <peeyuvi2002@gmail.com>
2025-07-30 12:23:08 +0530

    resolving invalid credentails issue 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,6 +29,8 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log('Login attempt with email:', email);
+
 
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: 'User not found' });
